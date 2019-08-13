886898c4dc490d7ba4a184d8568837b152ce213d
alumno <alumno@E314>
2019-08-13 07:50:11 -0700

    Clase Cuenta

---
+++ ModificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cuenta
+    {
+        public Cliente Propetario { get; set; }
+        public float Saldo { get; set; }
+
+        private string _identificador;
+        public string Identificador
+        {
+            get
+            {
+                return _identificador;
+            }
+            set
+            {
+                if(value.Length == 10 || value.Length == 11 || value.Length == 12 || value.Length == 13 || value.Length == 14)
+                {
+                    _identificador = value;
+                }
+                else
+                {
+                    throw new Exception("No es de 10 a 14 caracteres");
+                }
+            }
+        }
+
+
+
+    }
+}
